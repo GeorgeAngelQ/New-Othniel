b6e8c24ba0cd085c79b0f6addd43be44b28f1377
GeorgeAngel <quispemescuag@gmail.com>
2025-10-08 15:44:41 -0500

    Corrigiendo el rolemiddleware, impedimento a login y register

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,9 +23,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
         'role' => RoleMiddleware::class,
         ]);
-        $middleware->api(prepend: [
-            RoleMiddleware::class
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (ModelNotFoundException $e, $request) {
